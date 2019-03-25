51932e58fa6375d9ac007ddbd3aed85c9d2d2eb8
Jason Ritenour <jason.ritenour@gmail.com>
2019-03-25 17:10:12 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 0.11.1"
+}
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+resource "aws_instance" "ubuntu" {
+  ami           = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
+  availability_zone = "${var.aws_region}b"
+  associate_public_ip_address = "true"
+  tags {
+    Name = "${var.name}"
+    owner = "jritenour@hashicorp.com"
+    ttl = "24"
+  }
+}
